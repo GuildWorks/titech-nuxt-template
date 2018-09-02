148f342b99a672f17fe29bf76dc11796404c57b2
posaunehm <posaunehm@gmail.com>
2018-09-02 11:22:49 +0900

    firebase初期設定を追加

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "vuetify/dist/vuetify.min.css";
+import firebase from "firebase";
 
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: "AIzaSyD1-XpeZoFVg7H3MnRpWJLeSgqCzTg30P8",
+  authDomain: "titech-83c7f.firebaseapp.com",
+  databaseURL: "https://titech-83c7f.firebaseio.com",
+  projectId: "titech-83c7f",
+  storageBucket: "titech-83c7f.appspot.com",
+  messagingSenderId: "1027200745609"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
